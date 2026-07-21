c12caeb18b317c60207c1030f14fa28e126a2569
jsoadjedejosias <jsoadjedejosias@gmail.com>
2026-07-21 19:00:27 +0000

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -2,5 +2,5 @@ import { WebView } from "react-native-webview";
 import Constants from "expo-constants";
 export default function Home() {
   const url = Constants.expoConfig?.extra?.SITE_URL ?? "https://google.com";
-  return <WebView source={{ uri: url }} style={{ flex: 1, marginTop: 40 }} />;
+  return <WebView source={{ uri:https://genix-ambition-forge.vercel.app/ }} style={{ flex: 1, marginTop: 40 }} />;
 }
